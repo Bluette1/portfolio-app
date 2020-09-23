a864772dfd1109e7d9e8074d788efe574a5e32da
Bluette1 <marylene.sawyer@gmail.com>
2020-09-23 14:17:23 +0200

    Add the main page

---
+++ app/controllers/contents_controller.rb
@@ -0,0 +1,14 @@
+class ContentsController < ApplicationController
+  before_action :set_content, only: [:show, :edit, :update, :destroy]
+
+  # GET /contents
+ 
+  def index
+    
+  end
+
+  # GET /details
+  def show
+    
+  end
+end
