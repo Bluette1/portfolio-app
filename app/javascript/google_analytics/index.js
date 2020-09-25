4c70f1dc86e920b85e2cb6f6f01866fde04717c1
Bluette1 <marylene.sawyer@gmail.com>
2020-09-25 06:35:25 +0200

    Fix syntax error in application.html.erb

---
+++ app/javascript/google_analytics/index.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'UA-179041920-1');
+gtag('config', 'UA-179041920-2');
