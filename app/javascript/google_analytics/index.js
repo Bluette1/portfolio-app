1cc4b8a025f87a5c821eacbe89ed9107d3eeeaa4
Bluette1 <marylene.sawyer@gmail.com>
2020-09-25 07:55:41 +0200

    revert changes in `javascript` folder

--- app/javascript/google_analytics/index.js
+++
@@ -1,7 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-
-gtag('config', 'UA-179041920-1');
-gtag('config', 'UA-179041920-2');
